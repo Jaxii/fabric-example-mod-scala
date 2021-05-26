716544ecdd0141c206f4409fdf95ae87a28b5a48
BuildTools <jaxmontelongo@gmail.com>
2021-05-26 16:22:15 -0500

    Scala go brrr

---
+++ src/main/scala/net/fabricmc/example/ExampleMod.scala
@@ -0,0 +1,29 @@
+/*
+Theia: an open-source anti-cheat for fabric-modloader
+Copyright (C) 2021 Jaxii
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package net.fabricmc.example
+
+import net.fabricmc.api.ModInitializer
+
+
+class ExampleMod extends ModInitializer {
+
+
+	override def onInitialize(): Unit = {
+
+
+		print("it works!!!!!!!")
+
+
+	}
+
+
+}
